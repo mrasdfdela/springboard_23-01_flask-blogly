52d656bdba63708f5a93a9761e34fc544483e306
Jerry Hsu <Jerry>
2021-05-22 00:41:42 -0400

    Completed (and refactored) routes for user list, user details, adding users, editing users, and deleting users

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 # from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
@@ -15,18 +15,67 @@ app.config['SQLALCHEMY_ECHO'] = True
 connect_db(app)
 db.create_all()
 
+
 @app.route("/")
+def home():
+  return redirect("/users")
+
+
+@app.route("/users")
 def user_listing():
   users = User.get_all_users()
   return render_template("users.html", users=users)
 
-@app.route("/new_user")
-def new_user():
+
+@app.route("/users/new")
+def new_user_form():
   return render_template("new_user.html")
+
+
+@app.route("/users/new", methods=['POST'])
+def new_user_post():
+  req = request.form
+  first_name = req['first_name']
+  last_name = req['last_name']
+  image_url = req['image_url']
+
+  user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+  db.session.add(user)
+  db.session.commit()
+  return redirect(f"/user/{user.id}>")
+
 
 @app.route("/user/<int:user_id>")
 def user_detail(user_id):
   user = User.query.get(user_id)
-  # import pdb
-  # pdb.set_trace
   return render_template("user_detail.html",user=user)
+
+
+@app.route("/user/<int:user_id>/edit")
+def edit_user_form(user_id):
+  user = User.query.get(user_id)
+  return render_template("edit_user.html",user=user)
+
+
+@app.route("/user/<int:user_id>/edit", methods=["POST"])
+def edit_user(user_id):
+  req = request.form
+  first_name = req['first_name']
+  last_name = req['last_name']
+  image_url = req['image_url']
+
+  user = User.query.get(user_id)
+  user.first_name = first_name
+  user.last_name = last_name
+  user.image_url = image_url
+
+  db.session.commit()
+  return redirect(f"/user/{user.id}")
+
+
+@app.route("/user/<int:user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+  User.query.filter_by(id=user_id).delete()
+
+  db.session.commit()
+  return redirect(f"/users")
