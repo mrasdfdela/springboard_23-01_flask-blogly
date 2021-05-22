5ad4b544555005f2616a8f6d7d78c90ae920230d
Jerry Hsu <Jerry>
2021-05-21 21:30:29 -0400

    create flask app

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template, request
+# from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "jerrys_secret"
+# debug = DebugToolbarExtension(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
