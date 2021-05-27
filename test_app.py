5341ddfec2da34d825513eb3a7ea4c9d17ee3aa0
Jerry Hsu <Jerry>
2021-05-27 15:18:32 -0400

    Part 2, tests for post form, new post, edit post, and delete post. Also includes helper methods for creating a new user and corresponding new post.

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 from app import app
-from models import db, User
+from models import db, User, Post
 from unittest import TestCase
 
 app.config['TESTING'] = True
@@ -43,7 +43,7 @@ class UserDetailTestCase(TestCase):
 
 class NewUserTestCase(TestCase):
     def setUp(self):
-        self.user_id = addBarbara(self)
+        self.user_id = addBarbara()
 
     def tearDown(self):
         User.query.filter_by(id=self.user_id).delete()
@@ -57,12 +57,7 @@ class NewUserTestCase(TestCase):
             self.assertEqual(res.status_code, 200)
             self.assertIn("<h1>Barbara Streisand</h1>", html)
 
-# class NewPostTestCase(TestCase):
-#     def 
-# class EditPostTestCase(TestCase):
-# class DeletePostTestCase(TestCase):
-
-def addBarbara(self):
+def addBarbara():
     new_user = User(
               first_name="Barbara", 
               last_name="Streisand",
@@ -70,3 +65,81 @@ def addBarbara(self):
     db.session.add(new_user)
     db.session.commit()
     return new_user.id
+
+def addPost(user_id):
+    new_post = Post(
+      title = "Love",
+      content = "Soft as an easy chair",
+      user_id = user_id
+    )
+    db.session.add(new_post)
+    db.session.commit()
+    return new_post.id
+
+class NewPostTestCase(TestCase):
+    def setUp(self):
+        self.user_id = addBarbara()
+        # self.post_id = addPost(self.user_id)
+    def tearDown(self):
+        Post.query.filter_by(user_id=self.user_id).delete()
+        User.query.filter_by(id=self.user_id).delete()
+        db.session.commit()
+    
+    def test_new_post_form(self):
+        with app.test_client() as client:
+            res = client.get(f"/users/{self.user_id}/posts/new")
+            user = User.query.filter_by(id=self.user_id).first()
+            fname = user.first_name
+            lname = user.last_name
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn(f"<h1>Add Post for {fname} {lname}", html)
+    
+    def test_new_post(self):
+        with app.test_client() as client:
+            title = 'I am a woman in love'
+            res = client.post(
+              f'/users/{self.user_id}/posts/new',
+              data = {
+                'title': title,
+                'content': 'What do I do?',
+                'user_id': self.user_id
+              },
+              follow_redirects=True
+            )
+        html = res.get_data(as_text=True)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertIn(f'>{title}</a>',html)
+    
+    def test_edit_post(self):
+        with app.test_client() as client:
+            self.post_id = addPost(self.user_id)
+            new_title = "Edited Title"
+            res = client.post(
+              f'/posts/{self.post_id}/edit',
+              data = {
+                'title': new_title,
+                'content': "Edited Content"
+              },
+              follow_redirects=True
+            )
+        html = res.get_data(as_text=True)
+        
+        self.assertEqual(res.status_code,200)
+        self.assertIn(f'<h1>{new_title}</h1>',html)
+  
+    def test_delete_post(self):
+        user = User.query.get(self.user_id)
+        fname = user.first_name
+        lname = user.last_name
+
+        with app.test_client() as client:
+            self.post_id = addPost(self.user_id)
+            res = client.post(
+              f'/posts/{self.post_id}/delete',
+              follow_redirects=True
+            )
+        html = res.get_data(as_text=True)
+        self.assertIn(f'<h1>{fname} {lname}</h1>',html)
